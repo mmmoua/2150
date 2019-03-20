ccfdd570a7ccdbce452f693f3787d83c093cab06
mmmoua <48440748+mmmoua@users.noreply.github.com>
2019-03-20 13:06:46 -0400

    Add files via upload

---
+++ FileException.java
@@ -0,0 +1,9 @@
+public class FileException extends RuntimeException{
+    public FileException() {
+        super();
+    }
+    
+    public FileException(String msg) {
+        super(msg);
+    }
+}
