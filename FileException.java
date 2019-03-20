aa51662eacb99df88407642351907f6f79c80424
mmmoua <48440748+mmmoua@users.noreply.github.com>
2019-03-20 13:45:27 -0400

    Delete FileException.java

--- FileException.java
+++
@@ -1,9 +0,0 @@
-public class FileException extends RuntimeException{
-    public FileException() {
-        super();
-    }
-    
-    public FileException(String msg) {
-        super(msg);
-    }
-}
